7f4cdd1762df750dcb668b730b4cebdaac58bb36
Ted Elhourani <theodore.elhourani@gmail.com>
2018-04-07 21:41:39 -0700

    cleanup

--- main.c
+++ main.c
@@ -14,7 +14,6 @@ int show_tokens(token_t * tokens)
 
 int main(int argc, char **argv)
 {
-    //char * input = " r)) "; //"ferg)))";//" r)) ";  //"(begin\n (define r 10) (* pi (* r     r)) ) ";
     token_t* tokens = tokenize(argv[1]);
     show_tokens(tokens);
     return 0;
